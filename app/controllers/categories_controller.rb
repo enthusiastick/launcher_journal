a33831b239bb9cefb146bc54a0224c4763d93446
Eben <eben.lowe@gmail.com>
2013-12-23 15:37:15 -0500

    Added basic views (index, new, create) for categories.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ActionController::Base
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+     @category = Category.new(category_params)
+    @category.save
+    if @category.errors.any?
+      render action: 'new'
+    else
+      @categories = Category.all
+      render action: 'index'
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LauncherJournal::Application.routes.draw do
 
-  resources :entries
+  resources :entries, :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
