b21f7cdeda38bac757d8b841cfd85ed7f38c4415
Eben <eben.lowe@gmail.com>
2013-12-23 14:10:49 -0500

    Added new, create, show to controllers/views.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,25 @@
 class EntriesController < ActionController::Base
 
   def index
+    @entries = Entry.all
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    @entry.save
+    redirect_to @entry
+  end
+
+  def entry_params
+    params.require(:entry).permit(:title, :body)
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
   end
 
 end
